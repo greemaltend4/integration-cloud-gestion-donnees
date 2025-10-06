fc37674a3d33bb8b0dee321eed92a2072d47218c
greemaltend4 <dengestabiy@hotmail.com>
2025-10-06 18:12:27 +0000

    Replace deprecated MongoDB connection options with updated flags.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/integration-cloud', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000, // Allows 5 seconds to select an available server
 }).then(() => console.log('MongoDB connecté...')).catch(err => console.log(err));
 
 // Démarrez le serveur
