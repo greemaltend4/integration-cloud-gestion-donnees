4fb376d795e7019685ef50f377491778629d508c
greemaltend4 <dengestabiy@hotmail.com>
2025-10-06 18:12:12 +0000

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Bienvenue dans le projet d'intégration Cloud</h1>
+      <p>Gérez et analysez vos données à partir de différents services cloud.</p>
+    </div>
+  );
+};
+
+export default App;
